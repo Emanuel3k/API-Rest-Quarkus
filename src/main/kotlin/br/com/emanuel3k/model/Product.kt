baf93efeb27f3e106699bb91581026f0b9edb8d6
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-11-18 22:27:07 -0300

    correção de bug usando a anotação 'any'

--- src/main/kotlin/br/com/emanuel3k/model/Product.kt
+++ src/main/kotlin/br/com/emanuel3k/model/Product.kt
@@ -1,10 +1,14 @@
 package br.com.emanuel3k.model
 
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.enterprise.inject.Any
 
 @ApplicationScoped
 data class Product (
+    @Any
     val id: Long,
+    @Any
     val name: String,
+    @Any
     val price: Double
 )
