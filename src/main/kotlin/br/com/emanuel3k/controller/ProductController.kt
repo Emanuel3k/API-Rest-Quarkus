560e6bb2afaf58d4530f02d23a92a4f8f7e0a232
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-11-18 22:27:37 -0300

    Criação do controller e do primeiro metodo Post

---
+++ src/main/kotlin/br/com/emanuel3k/controller/ProductController.kt
@@ -0,0 +1,19 @@
+package br.com.emanuel3k.controller
+
+
+import br.com.emanuel3k.model.Product
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.POST
+import jakarta.ws.rs.Path
+
+@Path("/product")
+class ProductController {
+
+    private var products: MutableList<Product> = ArrayList()
+
+    @GET
+    fun getAllProducts(): MutableList<Product> {
+        return  products
+    }
+
+}
