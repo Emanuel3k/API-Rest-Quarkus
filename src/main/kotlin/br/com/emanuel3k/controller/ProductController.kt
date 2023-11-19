3fd8facf0ba4489a1256293f45c4f9c8880b7eee
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-11-18 22:45:01 -0300

    Criação do metodo que atualiza um produto com o param PUT

--- src/main/kotlin/br/com/emanuel3k/controller/ProductController.kt
+++ src/main/kotlin/br/com/emanuel3k/controller/ProductController.kt
@@ -4,7 +4,9 @@ package br.com.emanuel3k.controller
 import br.com.emanuel3k.model.Product
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.POST
+import jakarta.ws.rs.PUT
 import jakarta.ws.rs.Path
+import jakarta.ws.rs.PathParam
 
 @Path("/product")
 class ProductController {
@@ -13,13 +15,29 @@ class ProductController {
 
     @GET
     fun getAllProducts(): MutableList<Product> {
-        return  products
+        return products
     }
 
     @POST
-    fun postProduct(product: Product) : String {
+    fun postProduct(product: Product): String {
         products.add(product)
         return "Product registered successfully"
+    }
+
+    @PUT
+    @Path("/{id}")
+    fun putProduct(@PathParam("id") id: Long, product: Product): String {
+
+        products.stream()
+            .filter { p ->
+                p.id == id
+            }.findFirst()
+            .ifPresent { p ->
+                p.name = product.name
+                p.price = product.price
+            }
+
+        return "Product updated successfully"
     }
 
 }
